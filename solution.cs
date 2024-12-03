22d6e9196d820816b596eeae94d554dc8725288f
Kooreis <kooreis.official@gmail.com>
2024-12-03 03:30:36 -0600

    Added a function 'findFirstBadVersion' with two variables 'start' and 'end'.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,4 @@
-function isBadVersion(version) {
-    // This is a placeholder for the API call which would return whether the version is bad or not.
-    // In a real scenario, this function would make an API call to check if the version is bad.
-    // For the purpose of this problem, let's assume that versions higher than 5 are bad.
-    return version > 5;
+function findFirstBadVersion(n) {
+    let start = 1;
+    let end = n;
 }
